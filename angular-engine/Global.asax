7cd742c4e2c258d4dd60e73327c97868703c9b53
Argetim Ahmeti <argetima@cacttus.com>
2016-02-12 09:02:52 +0100

    sdf

---
+++ angular-engine/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="angular_engine.MvcApplication" Language="C#" %>
---
+++ angular-engine/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(angular_engine.Startup))]
+namespace angular_engine
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
